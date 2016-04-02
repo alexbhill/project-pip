e7330b318bb78fc323738894648c531ebf75a3d4
Aaron Petcoff <hello@aaronpetcoff.me>
2016-04-02 14:14:39 -0400

    add some comments

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,38 +2,31 @@ import Ember from 'ember';
 import _ from 'npm:lodash';
 import ENV from 'property-praxis/config/environment';
 
-const fields = [
-  'cartodb_id',
-  'count',
-  'layer',
-  'own_id',
-  'ownercity',
-  'ownerstate',
-  'ownerstr',
-  'propaddr',
-  'x',
-  'y',
-  'zipcount'
-];
-
 export default Ember.Route.extend({
   sqlService: Ember.inject.service('sql'),
 
   model() {
     const sql = this.get('sqlService'),
-      query = sql.prefix + encodeURIComponent(sql.model(fields)),
+      query = sql.prefix + encodeURIComponent(sql.model(sql.fields)),
       worker = new Worker(ENV.cartoWorker);
 
     return new Ember.RSVP.Promise(function (resolve) {
+      // fetch ajax & process data
+      // in a web worker
       worker.postMessage(query);
 
       worker.onmessage = function (e) {
+        // resolve the promise when the worker
+        // is done
         resolve(e.data);
       };
     });
   },
 
   afterModel() {
+    // always keep `model` clean
+    // modify `parcels` in the controller
+    // based on active & hidden layers
     this.controllerFor('index').set('parcels', this.modelFor('index'));
   }
 });
--- app/services/sql.js
+++ app/services/sql.js
@@ -1,12 +1,68 @@
 import Ember from 'ember';
 import _ from 'npm:lodash';
 
+/**
+ * Model and map data is stored in a a postGIS database
+ * on cartoDb.
+ *
+ * use ajax calls to get data from the db http://docs.cartodb.com/cartodb-platform/sql-api/making-calls/
+ *
+ * each row in the db is a new property.
+ *
+ * columns:
+ * cartodb_id - unique id for cartodb
+ * the_geom - geometric data for cartodb
+ * count - number of rows with this `own_id`
+ * layer - layers 0 - 5 depending on value of `count`
+ * legaldes - not used
+ * own_id - name of the owner of this property or the managing LLC
+ * own_type - i believe these are all "spec"
+ * ownercity - owner's business address city
+ * ownername1 - owner or managing llc
+ * ownername2 - secondary owner or managing llc
+ * ownerstate - owner's business address state
+ * ownerstr - owner's business address street
+ * ownerzip - owner's business address zip
+ * parcelno - city parcel number
+ * propaddr - address of property
+ * propclass - class number of property's legal desig.
+ * propdir - couldn't tell you
+ * propno - city's property number (distinct from parcel?)
+ * propstr - property street address
+ * propzip - property zip
+ * shape_star - parcel shape data
+ * shape_stle - parcel shape data
+ * subdiv - name of property's subdivision
+ * taxstatus - property tax status
+ * the_geom_webmercator - parcel shape data for carto
+ * ward - city's ward
+ * x - latitude
+ * y - longitude
+ * zipcount - number of rows with this `propzip`
+ */
+
 export default Ember.Service.extend({
   prefix: 'http://eightbitriot.cartodb.com/api/v2/sql?q=',
 
   default: 'select * from speculator_data',
 
+  fields: [
+    'cartodb_id',
+    'count',
+    'layer',
+    'own_id',
+    'ownercity',
+    'ownerstate',
+    'ownerstr',
+    'propaddr',
+    'x',
+    'y',
+    'zipcount'
+  ],
+
   model: function (fields) {
+    // only select fields from db as necessary
+    // for the model
     return `select ${fields.join(', ')} from speculator_data`;
   },
 
