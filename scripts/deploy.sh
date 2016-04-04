d3f64641ab96ecb54017e3e6fd3dfe709e41c986
Aaron Petcoff <hello@aaronpetcoff.me>
2016-04-04 09:38:47 -0400

    add scripts

---
+++ scripts/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+git com
+git branch -D gh-pages
+git push origin :gh-pages
+git new gh-pages
+source ~/.nvm/nvm.sh && nvm use
+node_modules/.bin/ember build --environment=production
+git add --all
+git commit -am "prod"
+git subtree push --prefix dist gh-pages
+git com
