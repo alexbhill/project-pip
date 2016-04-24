6a814db7824a1eaab727409b96d8efb267abb898
Aaron Petcoff <hello@aaronpetcoff.me>
2016-04-24 14:42:27 -0400

    link to streetview

--- app/components/streetview-loader.js
+++ app/components/streetview-loader.js
@@ -5,19 +5,22 @@ import _ from 'npm:lodash';
 export default Ember.Component.extend({
   src: null,
   alt: null,
+  href: null,
 
   classNames: ['streetview', 'panel-item'],
   classNameBindings: ['isLoaded'],
   isLoaded: false,
 
-  setSrc: Ember.observer('activeProperty', function () {
+  activeObserver: Ember.observer('activeProperty', function () {
     let active = this.get('activeProperty'),
       coordinates = active && [_.get(active, 'longitude'), _.get(active, 'latitude')];
 
     if (_.size(coordinates) && _.every(coordinates, _.isNumber)) {
-      this.set('src', streetviewUrl(coordinates));
+      this.set('src', setSrc(coordinates));
+      this.set('href', setHref(coordinates));
     } else {
       this.set('src', null);
+      this.set('href', null);
     }
   }),
 
@@ -29,12 +32,16 @@ export default Ember.Component.extend({
   handleLoad: Ember.observer('activeProperty', function(controller) {
     this.set('isLoaded', false); // re-init isLoaded to false on change
 
-    this.$('.streetview-container img').one('load', function() {
+    this.$().find('img').one('load', function() {
       controller.set('isLoaded', true);
     });
   })
 });
 
-function streetviewUrl (coordinates) {
-  return `https://maps.googleapis.com/maps/api/streetview?size=640x640&pitch=0&location=${coordinates.join(',')}&pitch=10&key=${ENV.STREETVIEW_KEY}`;
+function setSrc(coordinates) {
+  return `https://maps.googleapis.com/maps/api/streetview?size=640x640&location=${coordinates.join(',')}&pitch=10&key=${ENV.STREETVIEW_KEY}`;
+}
+
+function setHref(coordinates) {
+  return `http://maps.google.com/?cbll=${coordinates.join(',')}&layer=c`;
 }
