ed2f92a90447738087260f073c0374655477b100
Aaron Petcoff <hello@aaronpetcoff.me>
2016-05-02 11:06:49 -0400

    don't delay components from inserting

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,21 +5,23 @@ import ENV from 'property-praxis/config/environment';
 export default Ember.Route.extend({
   sqlService: Ember.inject.service('sql'),
 
-  model() {
+  // don't wait to insert components
+  // etc for large model. just load the
+  // model in the background
+  afterModel() {
     const sql = this.get('sqlService'),
       query = sql.prefix + encodeURIComponent(sql.model),
-      worker = new Worker(ENV.cartoWorker);
+      worker = new Worker(ENV.cartoWorker),
+      controller = this.controllerFor('index');
 
-    return new Ember.RSVP.Promise(function (resolve) {
-      // fetch ajax & process data
-      // in a web worker
-      worker.postMessage(query);
+    // fetch ajax & process data
+    // in a web worker
+    worker.postMessage(query);
 
-      worker.onmessage = function (e) {
-        // resolve the promise when the worker
-        // is done
-        resolve(e.data);
-      };
-    });
+    worker.onmessage = function (e) {
+      // resolve the promise when the worker
+      // is done
+      controller.set('model', e.data);
+    };
   }
 });
