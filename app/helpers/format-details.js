4540c51febebb05de6c8efa582989ec8c856f3cc
Aaron Petcoff <hello@aaronpetcoff.me>
2016-07-09 17:01:47 -0400

    don't import all of lodash

--- app/helpers/format-details.js
+++ app/helpers/format-details.js
@@ -1,10 +1,13 @@
 import Ember from 'ember';
-import _ from 'npm:lodash';
+import lowerCase from 'npm:lodash/lowerCase';
+import capitalize from 'npm:lodash/capitalize';
+import map from 'npm:lodash/map';
+import endsWith from 'npm:lodash/endsWith';
 
 export function formatDetails(params) {
-  const words = _.lowerCase(params.shift()).split(/\s/g),
-    fmt = _.map(words, _.capitalize).join(' '),
-    isLlc = _.endsWith(_.lowerCase(fmt), 'llc');
+  const words = lowerCase(params.shift()).split(/\s/g),
+    fmt = map(words, capitalize).join(' '),
+    isLlc = endsWith(lowerCase(fmt), 'llc');
 
   if (isLlc) {
     return `${fmt.substring(0, fmt.length - 4)} ${fmt.substring(fmt.length - 4).toUpperCase()}`;
