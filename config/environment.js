ffea6c65600fb4ffe542a3656fee726307d2d6e0
Aaron Petcoff <hello@aaronpetcoff.me>
2016-07-09 16:35:43 -0400

    location type = hash

--- config/environment.js
+++ config/environment.js
@@ -54,7 +54,7 @@ module.exports = function(environment) {
     modulePrefix: 'property-praxis',
     environment: environment,
     baseURL: '/',
-    locationType: 'auto',
+    locationType: 'hash',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
