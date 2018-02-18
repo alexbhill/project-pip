c926d89b5285a3206a7006424f803df943514821
Alex B. Hill <alexbrianhill@gmail.com>
2018-02-18 10:46:33 -0500

    single color gradient

--- app/services/styles.js
+++ app/services/styles.js
@@ -11,19 +11,19 @@ export default Ember.Service.extend({
     `\tline-opacity: 1;\n` +
   `}\n` +
   `#${table} [ ${mappings.layer} = 0 ] {\n` +
-    `\tpolygon-fill: #008aad;\n` +
-    `\tline-color: #004759;\n` +
+    `\tpolygon-fill: #feedde;\n` +
+    `\tline-color: #000;\n` +
   `}\n` +
   `#${table} [ ${mappings.layer} = 1 ] {\n` +
-    `\tpolygon-fill: #696684;\n` +
-    `\tline-color: #464459;\n` +
+    `\tpolygon-fill: #fdbe85;\n` +
+    `\tline-color: #000;\n` +
   `}\n` +
   `#${table} [ ${mappings.layer} = 2 ] {\n` +
-    `\tpolygon-fill: #ff935e;\n` +
-    `\tline-color: #995838;\n` +
+    `\tpolygon-fill: #fd8d3c;\n` +
+    `\tline-color: #000;\n` +
   `}\n` +
   `#${table} [ ${mappings.layer} = 3 ] {\n` +
-    `\tpolygon-fill: #ef554a;\n` +
-    `\tline-color: #99362F;\n` +
+    `\tpolygon-fill: #d94701;\n` +
+    `\tline-color: #000;\n` +
   `}`
 });
